7272425c18533f52274b6a1b7a3b7467789217fa
rsmith <legendary.belief@gmail.com>
2021-03-04 11:21:39 -0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    array = []
-    name_hash.each_pair {|key, value| array << value}
-    name_hash.key(array.sort.shift)
+    arr = []
+    name_hash.each_pair {|key, value| arr << value}
+    name_hash.key(arr.sort.shift)
 end
 
